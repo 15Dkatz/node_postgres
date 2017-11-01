e496b4eb07144a4fb64da867efe7ab6a57487782
David Katz <15Dkatz@shcp.edu>
2017-10-31 23:21:46 -0700

    Add monsters post, put, delete

--- monsters_api/app.js
+++ monsters_api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const monsters = require('./routes/monsters');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/monsters', monsters);
 
 app.use((err, req, res, next) => {
--- monsters_api/routes/monsters.js
+++ monsters_api/routes/monsters.js
@@ -21,4 +21,50 @@ router.get('/:id', (request, response, next) => {
   });
 });
 
+router.post('/', (request, response, next) => {
+  const { name, personality } = request.body;
+
+  pool.query(
+    'INSERT INTO monsters(name, personality) VALUES($1, $2)',
+    [name, personality],
+    (err, res) => {
+      if (err) return next(err);
+
+      response.redirect('/monsters');
+    }
+  );
+});
+
+router.put('/:id', (request, response, next) => {
+  const { id } = request.params;
+  const keys = ['name', 'personality'];
+  const fields = [];
+
+  keys.forEach(key => {
+    if (request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) => {
+    pool.query(
+      `UPDATE monsters SET ${field}=($1) WHERE id=($2)`,
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
+
+        if (index === fields.length - 1) response.redirect('/monsters');
+      }
+    )
+  });
+});
+
+router.delete('/:id', (request, response, next) => {
+  const { id } = request.params;
+
+  pool.query('DELETE FROM monsters WHERE id=($1)', [id], (err, res) => {
+    if (err) return next(err);
+
+    response.redirect('/monsters');
+  });
+});
+
 module.exports = router;
